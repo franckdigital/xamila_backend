b7f6daf3c73f6b18271d18d512790aef9079d707
franckdigital <franckalain.digital@gmail.com>
2025-11-23 19:54:15 +0000

    Update XAMILA_TEAM_EMAIL to xamila.developer@gmail.com

--- xamila/settings.py
+++ xamila/settings.py
@@ -217,7 +217,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Configuration SMTP pour Gmail
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'franckalain.digital@gmail.com')
-XAMILA_TEAM_EMAIL = 'team@xamila.com'
+XAMILA_TEAM_EMAIL = 'xamila.developer@gmail.com'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
